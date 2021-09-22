94377b4e2f9f5fef4e1462e0229c59dfe7bf1fe6
Olga-Go at knushka.com <olga@knushka.com>
2021-09-22 12:33:18 -0700

    Update v4

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V3!!!
+          Learn React V4!!!
         </a>
       </header>
     </div>
